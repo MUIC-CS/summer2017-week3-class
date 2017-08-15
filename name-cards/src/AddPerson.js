e20faf6eb1ad7841cc06c2731ba1785f05a894c3
Piti <piti118@gmail.com>
2017-08-15 14:42:37 +0700

    update

--- name-cards/src/AddPerson.js
+++ name-cards/src/AddPerson.js
@@ -2,20 +2,46 @@ import React, { Component } from 'react';
 
 export default class AddPerson extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      gender: 'male'
+    }
+  }
+
   onAddPersonClick() {
     console.log("AddPersonClick")
     this.props.onPersonAdd("Bright", "male")
+  }
+
+  onNameChange(e){
+    const newName = e.target.value
+    this.setState({name: newName})
+  }
+
+  onGenderChange(e) {
+    const newGender = e.target.value
+    this.setState({gender: newGender})
   }
 
   render() {
     return(
       <div>
         <div>
-        Name: <input type="text" />
+          Name:
+          <input
+            type="text"
+            value={this.state.name}
+            onChange={(e)=>this.onNameChange(e)}
+          />
         </div>
         <div>
           Gender:
-          <select>
+          <select
+            value={this.state.gender}
+            onChange={(e)=>this.onGenderChange(e)}
+          >
             <option value="male" > Male </option>
             <option value="female"> Female </option>
           </select>
